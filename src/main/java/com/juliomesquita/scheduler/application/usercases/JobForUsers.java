5a3983c562127be214c847d2671c2c25f6487619
juliocesarmcamilo@gmail.com <juliocesarmcamilo@gmail.com>
2025-06-09 18:42:01 -0300

    Implementação inicial do serviço

--- src/main/java/com/juliomesquita/scheduler/application/usercases/JobForUsers.java
+++ src/main/java/com/juliomesquita/scheduler/application/usercases/JobForUsers.java
@@ -2,6 +2,7 @@ package com.juliomesquita.scheduler.application.usercases;
 
 import com.juliomesquita.scheduler.domain.UserAggregate;
 import com.juliomesquita.scheduler.domain.entities.ProcessEntity;
+import com.juliomesquita.scheduler.domain.enums.ProcessStatus;
 import com.juliomesquita.scheduler.domain.repositories.ProcessRepository;
 import com.juliomesquita.scheduler.domain.repositories.UserRepository;
 import com.juliomesquita.scheduler.infra.serviceExternal.ProcessClient;
@@ -29,7 +30,7 @@ public class JobForUsers {
 
    @Scheduled(cron = "0 0 12 * * *") // "0 0 12 * * *" = todo dia às 12:00:00 (meio-dia)
    public void execute(){
-      final List<UserAggregate> users = this.userRepository.findAll();
+      final List<UserAggregate> users = this.userRepository.findByStatus(ProcessStatus.PENDING);
       users.forEach(user -> {
          final ProcessClientResponse responseClient = this.processClient.getInfoProcess(user.getId());
 
--- src/main/java/com/juliomesquita/scheduler/domain/repositories/UserRepository.java
+++ src/main/java/com/juliomesquita/scheduler/domain/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.juliomesquita.scheduler.domain.repositories;
 
 import com.juliomesquita.scheduler.domain.UserAggregate;
+import com.juliomesquita.scheduler.domain.enums.ProcessStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserAggregate, Long> {
+   List<UserAggregate> findByStatus(ProcessStatus status);
 }
